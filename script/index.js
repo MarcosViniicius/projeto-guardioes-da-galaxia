489ad5ab8218d0c5c063576a69b4d29338cf0807
Marcos Vinicius de lucena <marcosvinicius.lucena27@gmail.com>
2024-02-07 11:41:32 -0300

    Alterações visuais e funcionais
    -Menu de personagens adicionado no canto superior direito da página -Função de trocar nome dos personagens adicionada.

--- script/index.js
+++ script/index.js
@@ -11,6 +11,7 @@ let activeMember = 0;
 const images = document.getElementById("images");
 const menu = document.getElementById("menu");
 const memberName = document.getElementById("member__name");
+const navigation = document.getElementById("navigation");
 
 function changeStatusButton() {
   const prev = document.getElementById("button-prev");
@@ -21,16 +22,26 @@ function changeStatusButton() {
   next.disabled = isLast;
 }
 
-function navigationMember(direction) {
-  activeMember = activeMember + direction;
+function changeMember(memberId) {
+  activeMember = memberId;
 
   const member = members[activeMember];
   console.log(activeMember, member);
   images.style.transform = `translateY(${-100 * activeMember}vh)`;
   memberName.classList = member.id;
+  changeName(member.name);
+
   changeStatusButton();
+}
+function navigationMember(direction) {
+  changeMember(activeMember + direction);
 }
 
 function changeMenu() {
   menu.classList.toggle("active");
+  navigation.classList.toggle("active");
+}
+
+function setMember(memberId) {
+  changeMember(memberId);
 }
